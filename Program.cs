df61e191613d85facf2d91a2ab3be36ec86b001b
Danny <dhinds423@gmail.com>
2021-11-03 14:13:26 -0500

    all menu options working - still need to add comments, loggers, and some exception handling

--- Program.cs
+++ Program.cs
@@ -58,30 +58,58 @@ namespace DotNetEFLAB
                         Console.WriteLine(item.BlogId+") " + item.Name);
                     }
                     blogChoice = Convert.ToInt32(Console.ReadLine());
-                    
-                    try{
-                        foreach (var item in query)
-                        {
-                            if (item.BlogId == blogChoice){
-                                Console.Write("Enter the Post title: ");
-                                var title = Console.ReadLine();
-                                Console.Write("Enter the Post content: ");
-                                var content = Console.ReadLine();
+                    var isValidBlogId = db.Blogs.Any(b => b.BlogId == blogChoice);
+                    if (isValidBlogId) 
+                    {
+                        Console.Write("Enter the Post title: ");
+                        var title = Console.ReadLine();
+                        Console.Write("Enter the Post content: ");
+                        var content = Console.ReadLine();
 
-                                var post = new Post { Title = title, Content = content };
+                        var post = new Post { Title = title, Content = content, BlogId = blogChoice };
 
-                                db.AddPost(post);
-                                logger.Info("Post added - {title}", title);
-                            }
-                        }   
-                    }catch (Exception ex){
-                        logger.Error(ex.Message);
+                        db.AddPost(post);
+                        logger.Info("Post added - {title}", title);
+                    } else {
+                        // TODO: invalid blog id
+                        Console.WriteLine("invalid blog id");
                     }
+                    
                 }
                 else if(choice == "4")
                 {
-                    
-
+                    var query = db.Blogs.OrderBy(b => b.BlogId);
+                    int postChoice;
+                    Console.WriteLine("Select the blog's posts to display");
+                    Console.WriteLine("0) Posts from all blogs");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.BlogId+") " + item.Name);
+                    }
+                    postChoice = Convert.ToInt32(Console.ReadLine());
+                    var isValidBlogId2 = db.Blogs.Any(b => b.BlogId == postChoice);
+                    if(postChoice == 0)
+                    {
+                        var posts = db.Posts.OrderBy(p => p.Title);
+                        Console.WriteLine((posts.Count())+" post(s) returned");
+                        foreach (var ps in posts)
+                        {
+                            Console.WriteLine("Blog: " + ps.Blog.Name);
+                            Console.WriteLine("Title: " + ps.Title);
+                            Console.WriteLine("Content: " + ps.Content+"\n");
+                        }
+                    }
+                    else if (isValidBlogId2)
+                    {
+                        var thisPost = db.Posts.Where(b => b.BlogId == postChoice);
+                        Console.WriteLine((thisPost.Count())+" post(s) returned");
+                        foreach (var ps in thisPost){
+                            
+                            Console.WriteLine("Blog: " + ps.Blog.Name);
+                            Console.WriteLine("Title: " + ps.Title);
+                            Console.WriteLine("Content: " + ps.Content+"\n");
+                        }
+                    }
                 }
 
             }while(choice == "1" || choice == "2" || choice == "3" || choice == "4");
